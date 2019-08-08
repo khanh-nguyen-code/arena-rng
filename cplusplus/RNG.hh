b563084c09473e530929c1066de61cc09eab390a
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-08-08 13:36:25 +0800

    modified:   RNG.hh 	modified:   src/RNG.hh

--- cplusplus/RNG.hh
+++ cplusplus/RNG.hh
@@ -2,8 +2,10 @@
 #define _RNG_PUBLIC_HH_
 #include<array>
 struct RNG {
-	std::array<int64_t, 8> _; // padding
+	// no need padding here
 	RNG(const std::array<int64_t, 6>& seed);
 	double Next();
+	// padding (not necessary)
+	std::array<int64_t, 8> _;
 };
 #endif
--- cplusplus/src/RNG.hh
+++ cplusplus/src/RNG.hh
@@ -2,12 +2,11 @@
 #define _RNG_HH_
 #include<cstdint>
 #include<array>
-class RNG {
+struct RNG {
+	RNG(const std::array<int64_t, 6>& seed);
+	double Next();
 private:
 	std::array<int64_t, 4> a;
 	std::array<int64_t, 4> b;
-public:
-	RNG(const std::array<int64_t, 6>& seed);
-	double Next();
 };
 #endif
