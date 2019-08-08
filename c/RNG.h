5251251362b3091e2794ee21d033da4a71f4487b
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-08-08 13:23:30 +0800

    modified:   RNG.h

--- c/RNG.h
+++ c/RNG.h
@@ -1,9 +1,7 @@
 #ifndef _RNG_PUBLIC_H_
 #define _RNG_PUBLIC_H_
 #include<stdint.h>
-struct rng;
-struct rng_trait;
-typedef struct RNG RNG;
+typedef struct rng RNG;
 typedef struct rng_trait RNG_TRAIT;
 struct rng_trait {
   RNG* (*New)(int64_t seed[6]);
