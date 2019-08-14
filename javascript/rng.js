714548e94dfec4c7254557370218d5692976d9a9
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-08-14 20:36:18 +0800

    modified:   rng.js

--- javascript/rng.js
+++ javascript/rng.js
@@ -1,12 +1,18 @@
 class RNG {
     constructor(seed) {
-        this.a = [0, seed[0], seed[1], seed[2]];
-        this.b = [0, seed[3], seed[4], seed[5]];
+        this._a = [0, seed[0], seed[1], seed[2]];
+        this._b = [0, seed[3], seed[4], seed[5]];
+    }
+    get a() {
+        return this._a;
+    }
+    get b() {
+        return this._b;
     }
     Next() {
-        this.a[0] = (1403580 * this.a[2] - 810728 * this.a[3]) % 4294967087;
-        this.b[0] = (527612 * this.b[1] - 1370589 * this.b[3]) % 4294944443;
-        var z = (this.a[0] - this.b[0]) % 4294967087;
+        this._a[0] = (1403580 * this._a[2] - 810728 * this._a[3]) % 4294967087;
+        this._b[0] = (527612 * this._b[1] - 1370589 * this._b[3]) % 4294944443;
+        var z = (this._a[0] - this._b[0]) % 4294967087;
         if (z < 0) {
             z += 429467087;
         }
@@ -14,15 +20,14 @@ class RNG {
         if (z > 0) {
             u = z / 4294967087.0;
         }
-        this.a[3] = this.a[2];
-        this.a[2] = this.a[1];
-        this.a[1] = this.a[0];
-        this.b[3] = this.b[2];
-        this.b[2] = this.b[1];
-        this.b[1] = this.b[0];
+        this._a[3] = this._a[2];
+        this._a[2] = this._a[1];
+        this._a[1] = this._a[0];
+        this._b[3] = this._b[2];
+        this._b[2] = this._b[1];
+        this._b[1] = this._b[0];
         return u;
     };
-
 }
 
 function main() {
