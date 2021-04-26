5a6b0b3cd6c95eb0ed28cdd014f3c845163a0a75
Khanh Nguyen <khanh.nguyen@shopee.com>
2021-04-26 14:50:51 +0800

    added cpp

--- c/RNG.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RNG_H_
-#define _RNG_H_
-#include<stdint.h>
-struct rng_state {
-  int64_t a[4];
-  int64_t b[4];
-};
-double rng_next(struct rng_state*);
-#endif
---
+++ cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "rng.h"
+
+int main() {
+    auto s = rng({1, 2, 3, 4, 5, 6});
+    for (int i=0; i<100; i++) {
+        std::cout << s.next() << std::endl;
+    }
+    return 0;
+}
