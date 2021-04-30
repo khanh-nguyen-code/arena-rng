572fb4326bd070068f0ef307ae4bfd179f8a0137
Khanh Nguyen <khanh.nguyen@shopee.com>
2021-04-30 16:53:19 +0800

    fixed python

--- rng.py
+++ rng.py
@@ -15,7 +15,7 @@ def RNG(*args: int) -> Iterator[float]:
         a = (1403580 * a_list[1] + (4294967087 - 810728) * a_list[2]) % 4294967087
         b = (527612 * b_list[0] + (4294944443 - 1370589) * b_list[2]) % 4294944443
         # z always in range [1, 4294967087]
-        z = (a + 4294967087 - b) % 4294967087 + 1  # shifted by +1 as compare to arena
+        z = (a + 4294967087 - b) % 4294967087 + 1  # shifted by +1 comparing to arena
         u = float(z) / 4294967088.0
         # shift
         a_list = [a, *a_list[0:2]]
@@ -25,5 +25,5 @@ def RNG(*args: int) -> Iterator[float]:
 
 if __name__ == "__main__":
     rng = RNG(1, 2, 3, 4, 5, 6)
-    for i in range(10):
-        print(next(rng))
+    for i, rand in zip(range(100), rng):
+        print(f"{i}: {rand}")
