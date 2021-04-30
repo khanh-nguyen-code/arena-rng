cceb16c8dda61576c2366ad28ec542d5c250055e
Khanh Nguyen <khanh.nguyen@shopee.com>
2021-04-30 15:43:48 +0800

    fixed

--- rng.py
+++ rng.py
@@ -5,7 +5,7 @@ def ArenaRNG(*args: int) -> Iterator[float]:
     assert len(args) == 6, "seed must be 6 non-negative integers less than 2^32"
     for seed in args:
         assert isinstance(seed, int), "seed must be 6 non-negative integers less than 2^32"
-        assert 0 <= seed <= 2**32, "seed must be 6 non-negative integers less than 2^32"
+        assert 0 <= seed < 2**32, "seed must be 6 non-negative integers less than 2^32"
 
     a_list = [*args[0:3]]
     b_list = [*args[3:6]]
@@ -13,6 +13,7 @@ def ArenaRNG(*args: int) -> Iterator[float]:
         a = (1403580 * a_list[1] + (4294967087 - 810728) * a_list[2]) % 4294967087
         b = (527612 * b_list[0] + (4294944443 - 1370589) * b_list[2]) % 4294944443
         # a, b always in range [0, 2^32)
+        assert 0 <= a < 2**32 and 0 <= b < 2**32
         z = (a + 4294967087 - b) % 4294967087
         if z == 0:
             # set z to 4294967087 if z == 0
