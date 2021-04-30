de94f2397649a6dac673d251597758c82b658fb5
Khanh Nguyen <khanh.nguyen@shopee.com>
2021-04-30 15:08:53 +0800

    added c

--- c/main.c
+++ c/main.c
@@ -5,8 +5,9 @@ int main() {
     int64_t seed[6] = {1, 2, 3, 4, 5, 6};
     struct rng_state state = rng_state_new(seed);
     for (int i=0; i<10; i++) {
-        state = rng_state_next(state);
-        printf("%f\n", state.u);
+        struct rng_result result = rng_state_next(state);
+        state = result.state;
+        printf("%f\n", result.value);
     }
     return 0;
 }
