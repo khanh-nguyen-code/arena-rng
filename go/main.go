73d63fb142ed8ea483d2b0d1feb5e802b3eeddab
Khanh Nguyen <khanh.nguyen@shopee.com>
2021-04-30 15:19:42 +0800

    fixed go

--- go/main.go
+++ go/main.go
@@ -7,8 +7,10 @@ import (
 
 func main() {
 	seed := [6]int64{1, 2, 3, 4, 5, 6}
-	gen := rng.NewArenaRNG(seed)
+	state := rng.New(seed)
+	var value float64
 	for i := 0; i < 10; i++ {
-		fmt.Println(gen.Next())
+		state, value = state.Next()
+		fmt.Println(value)
 	}
 }
--- go/rng/rng.go
+++ go/rng/rng.go
@@ -1,14 +1,29 @@
 package rng
 
-// RNG : Random Number Generator
-type RNG interface {
-	Next() float64
+// ArenaState : Random Number Generator
+type ArenaState struct {
+	a [3]int64
+	b [3]int64
 }
 
-// NewArenaRNG : create new RNG
-func NewArenaRNG(seed [6]int64) RNG {
-	return &arenaRNG{
-		[4]int64{0, seed[0], seed[1], seed[2]},
-		[4]int64{0, seed[3], seed[4], seed[5]},
+func New(seed [6]int64) ArenaState {
+	return ArenaState{
+		a: [3]int64{seed[0], seed[1], seed[2]},
+		b: [3]int64{seed[3], seed[4], seed[5]},
 	}
+}
+
+func (state ArenaState) Next() (nextState ArenaState, value float64) {
+	var a int64 = (1403580*state.a[1] - 810728*state.a[2]) % 4294967087 // might be negative
+	var b int64 = (527612*state.b[0] - 1370589*state.b[2]) % 4294944443 // might be negative
+	var z int64 = (a - b) % 4294967087
+	if z <= 0 {
+		// positivify, set z to 4294967087 if z == 0
+		z += 4294967087
+	}
+	value = float64(z) / 4294967088.0
+	return ArenaState{
+		a: [3]int64{a, state.a[0], state.a[1]},
+		b: [3]int64{b, state.b[0], state.b[1]},
+	}, value
 }
