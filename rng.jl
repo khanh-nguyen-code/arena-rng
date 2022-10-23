f54526b94c9cbd43bd4cc70d2d455e2d76d67558
khanh nguyen <khanh.nguyen.contact@gmail.com>
2022-10-24 02:00:14 +0800

    m

--- rng.jl
+++ rng.jl
@@ -12,7 +12,7 @@ module RNG
         )
     end
 
-    function next(state::State)::Tuple{Float64, State}
+    function next(state::State)::Tuple{State, Float64}
         # each component always in range [0, 2^64)
         # a, b always in range [0, 2^32)
         a = (1403580 * state.a[2] + (4294967087 - 810728) * state.a[3]) % 4294967087
@@ -20,8 +20,9 @@ module RNG
         # z always in range [1, 4294967087]
         z = (a + 4294967087 - b) % 4294967087 + 1 # shifted by +1 comparing to arena
         u = Float64(z) / 4294967088.0
+        next_state = State((a, state.a[1], state.a[2]), (b, state.b[1], state.b[2]))
         # return
-        u, State((a, state.a[1], state.a[2]), (b, state.b[1], state.b[2]))
+        next_state, u
     end
 
     # iteration
@@ -33,7 +34,12 @@ module RNG
     end
 end
 
-rng = RNG.New(1, 2, 3, 4, 5, 6)
-for (i, rand) in zip(1:100, rng)
-    println("$i: $rand")
+function main()
+    state = RNG.New(1, 2, 3, 4, 5, 6)
+    for i in 1:100
+        state, u = RNG.next(state)
+        println("$i: $u")
+    end
 end
+
+main()
